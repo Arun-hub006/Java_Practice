e213c7110d94fcb412c6e3abf44afc917f2ba9c6
Arun-hub006 <shadowarun2006@gmail.com>
2026-04-23 12:06:01 +0530

    first commit

---
+++ hellow.java
@@ -0,0 +1,8 @@
+public class hellow {
+    public static void main(String[] args) {
+        int a = 18;
+        if (a  >1) 
+            System.out.println("You are an adult");
+         
+    }
+}
