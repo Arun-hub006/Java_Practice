92b69920e1bea7e447ea530261a1ac996840755b
Arun-hub006 <shadowarun2006@gmail.com>
2026-04-27 19:44:56 +0530

    firstt

--- Array.java
+++ Array.java
@@ -1,7 +1,14 @@
 public class Array {
     public static void main (String args[]) {
-        int array[]= {1,2,3,4,5};
-        System.out.println(array[0]);
-        
+        int array[] = new int[5];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+        for(int i=0;i<=4;i++){
+            System.out.println(array[i]);
+        }
+       
     }
 }
